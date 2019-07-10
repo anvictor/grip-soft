57a5f1b00ee2f3eff5cdbffc053deedc622fc08d
Viktor <anvictor0@gmail.com>
2019-07-10 17:06:54 +0300

    redux ok routing start

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Customers from './components/customers/customers.component';
 import Navbar from 'react-bootstrap/Navbar'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Products from "./components/products/products.component";
 import Invoices from "./components/invoices/invoices.component";
 import "./App.css";
+import getCustomers from './actions/customers';
+
 function App(props) {
   console.log('App', props);
-  function onAddNewProduct(e) {props.onAddProduct(e)}
+  function InvoicesRoute() {return <Invoices onAddInvoice={onAddNewInvoice} invoices = {props.invoices} />}
+  function ProductsRoute() {return <Products onAddProduct={onAddNewProduct} products = {props.products} />}
+  function CustomersRoute() {return <Customers onAddCustomer={onAddNewCustomer} customers = {props.customers}/>}
+
   function onAddNewInvoice(e) {props.onAddInvoice(e)}
+  function onAddNewProduct(e) {props.onAddProduct(e)}
   function onAddNewCustomer(e) {props.onAddCustomer(e)}
   return (
-    <div>
+    <Router>
+
+
       <Navbar expand="lg" variant="light" bg="light">
-        <h5>Invoice App</h5>
+        <h2>Invoice App</h2>
         <Navbar.Brand href="/invoices">Invoices</Navbar.Brand>
         <Navbar.Brand href="/products">Products</Navbar.Brand>
-        <Navbar.Brand href="/customers">Customers</Navbar.Brand>
+        <Navbar.Brand onClick={props.onGetCustomers} href="/customers">Customers</Navbar.Brand>
       </Navbar>
-      <Customers onAddCustomer={onAddNewCustomer} customers = {props.customers}/>
-      <Products onAddProduct={onAddNewProduct} products = {props.products} />
-      <Invoices onAddInvoice={onAddNewInvoice} invoices = {props.invoices} />
-    </div>
+      <Route path="/invoices/" exact component={InvoicesRoute} />
+      <Route path="/products/" component={ProductsRoute} />
+      <Route path="/customers/" component={CustomersRoute} />
+      {/*<Invoices onAddInvoice={onAddNewInvoice} invoices = {props.invoices} />*/}
+      {/*<Products onAddProduct={onAddNewProduct} products = {props.products} />*/}
+      {/*<Customers onAddCustomer={onAddNewCustomer} customers = {props.customers}/>*/}
+    </Router>
   );
 }
 
@@ -34,6 +47,7 @@ export default connect(
   dispatch =>({
     onAddProduct(e){dispatch({type: 'ADD_PRODUCT', payload: e});},
     onAddInvoice(e){dispatch({type: 'ADD_INVOICE', payload: e});},
-    onAddCustomer(e){dispatch({type: 'ADD_CUSTOMER', payload: e});}
+    onAddCustomer(e){dispatch({type: 'ADD_CUSTOMER', payload: e});},
+    onGetCustomers: () => {dispatch(getCustomers());}
   })
 )(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
 import customers from './customers';
 import products from './products';
 import invoices from './invoices';
 export default combineReducers({
+  routing: routerReducer,
   customers,
   products,
   invoices
